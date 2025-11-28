2ac91cd5da1822c383c7703cb178be461437b1d0
Munechi <github@velvet.sakura.ne.jp>
2025-11-28 16:30:58 +0900

    Fix: 引数の型チェックを追加

--- src/sample_repo/main.py
+++ src/sample_repo/main.py
@@ -8,4 +8,6 @@ def add(a: int, b: int) -> int:
     Returns:
         (int): 計算結果
     """
-    return int(a) + int(b)
+    if not isinstance(a, int): raise TypeError("a is not integer.")
+    if not isinstance(b, int): raise TypeError("b is not integer.")
+    return a + b
