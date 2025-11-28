5756a1ddc1b64abf0c4619b5c2752d4e7f59e5b6
Munechi <github@velvet.sakura.ne.jp>
2025-11-28 16:06:11 +0900

    Fix: review feedback

--- src/sample_repo/main.py
+++ src/sample_repo/main.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+def add(a: int, b: int) -> int:
+    """整数の足し算
+
+    Args:
+        a (int): 計算値a
+        b (int): 計算値b
+
+    Returns:
+        (int): 計算結果
+    """
+    return int(a) + int(b)
